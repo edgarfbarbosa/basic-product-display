4d737821b70bd9a5e82e5155b627349b8e439f52
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-05-09 12:43:33 -0300

    refactor: refatora código para melhor legibilidade

--- api/index.js
+++ api/index.js
@@ -6,19 +6,30 @@ const http = require('http')
 // Lê o arquivo data.json e o converte para um objeto JavaScript
 const data = JSON.parse(fs.readFileSync(`${__dirname}/data.json`, 'utf-8'))
 
+// Cria um servidor HTTP para atender as requisições do cliente
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-  })
-  res.end(JSON.stringify(data))
+  // Obtém o caminho da requisição, que é a parte da URL após o hostname e porta
+  const pathname = req.url
+  
+  // Verifica o caminho da requisição
+  switch (pathname) {
+    // Se o caminho da URL for "/api", executa o seguinte bloco de código
+    case '/api':
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+    // Responde o cliente enviando os dados do arquivo data.json no formato JSON
+    res.end(JSON.stringify(data))
+    break
+  }
 })
 
 const port = 8000
 const hostname = '127.0.0.1'
 /** 
-* Inicia o servidor HTTP para atender as requisições.
-* @param {number} port - Porta em que o servidor vai atender as requisições.
-* @param {string} hostname - Endereço de IP que vai atender as requisições.
+* Inicia o servidor HTTP para atender as requisições do cliente.
+* @param {number} port - Porta em que o servidor vai atender as requisições do cliente.
+* @param {string} hostname - Endereço de IP que vai atender as requisições do cliente.
 * @param {Function} callback - Função que será executada quando o servidor começar a escutar.
 */
 server.listen(port, hostname, () => {
